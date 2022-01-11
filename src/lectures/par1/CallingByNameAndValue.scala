f9d37e73bcba17f85d2b003d2966f29afa20d166
Petro Prydorozhnyi <petro@stakelogic.com>
2022-01-11 12:09:54 +0200

    call by name and value

--- src/lectures/par1/CallingByNameAndValue.scala
+++ src/lectures/par1/CallingByNameAndValue.scala
@@ -2,4 +2,18 @@ package lectures.par1
 
 object CallingByNameAndValue extends App {
 
+  def callByValue(x: Long): Unit = {
+    println("by value: " + x);
+    println("by value: " + x);
+  }
+
+  // looks like pass function in Java
+  def callByName(x: => Long): Unit = {
+    println("by name: " + x);
+    println("by name: " + x);
+  }
+
+  callByValue(System.nanoTime())
+  callByName(System.nanoTime())
+
 }
